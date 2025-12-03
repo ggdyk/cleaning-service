89f718d1fa0ee3b2074d463998b4673c117cb68c
Damir Ussenov <kiba_damir@mail.ru>
2025-12-03 21:14:44 +0500

    docker and postgresql connect

---
+++ src/Infrastructure/Persistence/ApplivationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
