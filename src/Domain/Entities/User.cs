ca188771bec8d6f811072f7a80c2efb64122ce11
Damir Ussenov <kiba_damir@mail.ru>
2025-12-05 18:20:42 +0500

    Add entities

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -2,5 +2,14 @@ namespace Domain.Entities;
 
 public class User
 {
-    
+    public Guid Id { get; } // уникальный id
+    public string Email { get; set; } = default!; // узнать что это?
+    public string PasswordHash { get; set; } = default!;
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Phone { get; set; } = default!;
+    public string Role { get; set; } = "client"; // клиент или админ. как вариант сделать enum???
+    public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+    public bool IsActive { get; set; } = true;
+    public string City { get; set; } //для будущего расширения
 }
